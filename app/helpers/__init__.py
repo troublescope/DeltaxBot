16fd4ad3af1e159b185051e5d0991c71a73e4853
troublescope <tomiprs25@gmail.com>
2025-03-10 15:59:05 +0700

    4feat(helpers): add catbox helper function to upload images to catbox.moe CDN
    Signed-off-by: troublescope <tomiprs25@gmail.com>

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,5 +1,6 @@
 from .button import ButtonMaker
 from .user import parse_info
+from .cdn_tools import catbox
 
 
-__all__ = ["ButtonMaker", "parse_info"]
+__all__ = ["ButtonMaker", "catbox", "parse_info"]
